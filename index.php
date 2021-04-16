a2241cd40b18e449d1dc57d9954c79d17ee10b79
ryuto1246 <ryutonakamura1246@gmail.com>
2021-04-16 22:21:04 +0900

    [add] skyway

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Skyway Test</title>
+  <script src="https://cdn.webrtc.ecl.ntt.com/skyway-4.4.1.js"></script>
+</head>
+<body>
+  <script>
+    
+  </script>
+</body>
+</html>
